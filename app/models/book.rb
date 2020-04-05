a96707e1578e6183babba40affc619a2c123a0f9
Dimmus <dmitri.chudinov@gmail.com>
2020-04-05 21:34:25 +0500

    try add _partial files

---
+++ app/models/book.rb
@@ -0,0 +1,9 @@
+# app/models/book.rb
+class Book < ApplicationRecord
+    has_many :shelves, dependent: :destroy
+    has_many :users, through: :shelves
+    scope :within_shelf, ->(place,user) {
+      joins(:shelves,:users)
+      .where(shelves: {place: place, users: {id: user.id}})
+    }
+end
---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+# app/models/user.rb
+class User < ApplicationRecord
+    attr_accessor :name
+    attr_accessor :image
+    has_many :shelves, dependent: :destroy
+    has_many :books, through: :shelves
+end
